b00fdb82cdc96f0e200f401a396acffdb091e1c2
350325066 <350325066@C-87YLND2.tdsb.on.ca>
2026-05-15 12:02:07 -0400

    325

---
+++ src/AlgorithmCase.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author fuche
+ */
+class AlgorithmCase extends EthicsCase {
+    private String biasType;
+    AlgorithmCase(String caseTitle, String description, String biasType){
+        super(caseTitle, description, "AlgorithmCase");
+        this.biasType = biasType;
+    }
+    public String getBiasType(){
+        return biasType;
+    }
+    @Override
+    public String toString(){
+        return super.toString() + ", bias type: " + biasType;
+    }
+}
